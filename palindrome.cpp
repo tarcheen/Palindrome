b96a61bc14735da4966113c4367482dca2210b2f
Hamed <hamed0311@gmail.com>
2019-09-17 15:44:32 -0700

    final working version pushed

---
+++ palindrome.cpp
@@ -0,0 +1,39 @@
+/*
+Developer: Hamed Mirlohi
+Date: 09/11/2019
+In this program we are checking whether a number is palindrome or not.
+Logic is to see if the number matches its reverse
+*/
+
+#include <iostream>
+
+// reversing a number
+int reverse_number(int number)
+{
+    int result = 0;
+    int remain;
+    while(number > 0)
+    {
+        remain = number % 10;
+        result = result * 10 + remain;
+        number /= 10;
+    }
+    return result;
+}
+
+bool isPalindrome(int number)
+{
+    return (reverse_number(number) == number) ? true : false;
+}
+
+
+int main(void)
+{
+    int number = 33;
+    if(isPalindrome(number))
+        std::cout<< number << " is Palindrome\n";
+    else
+        std::cout<< number << " is not Palindrome\n";
+    
+}
+
